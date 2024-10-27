bca0789fe434ee7b2103f00587e635e8f3b0e883
Neil de Carteret <git@lumphammer.net>
2024-10-27 14:07:30 +0000

    fix actions

--- scripts/build-actions.sh
+++ scripts/build-actions.sh
@@ -12,7 +12,9 @@ for input in $inputs; do
   src_dir=$(dirname $input)
   output_dir=$(dirname $src_dir)/dist
   echo -e "---\nInput file: ${input}\nOutput dir: ${output_dir}"
+  # see https://github.com/evanw/esbuild/issues/1921#issuecomment-2302290651
   npx esbuild ${input} \
+    --banner:js="import { createRequire } from 'module'; const require = createRequire(import.meta.url);" \
     --bundle \
     --format=esm \
     --target=node20 \

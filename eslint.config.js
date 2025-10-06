7aa0df4bac1cfbefb6775a0e58f471af8f7622cb
Neil de Carteret <git@lumphammer.net>
2025-10-06 11:50:15 +0100

    install eslint 9

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "eslint/config";
+import coreConfig from "@lumphammer/shared-fvtt-bits/dotfiles/import/eslint.core.config.js";
+
+const config = defineConfig([coreConfig]);
+
+export default config;
